55e3ddcbf81db93535c2aaee4c27f5d720570b12
rela589n <zsidelnik@gmail.com>
2020-01-30 22:02:03 +0200

    Print list of tests

--- resources/views/layouts/blocks/test-line.blade.php
+++ resources/views/layouts/blocks/test-line.blade.php
@@ -1,6 +1,6 @@
 <li class="list-group-item d-flex justify-content-between align-items-center">
-    <h3 class="category-header">{{ $test['name'] }}</h3>
+    <h3 class="category-header">{{ $test->name }}</h3>
 
-    <a href="{{ route($testRouteName, ['subject' => $test->subject, 'test' => $test['uri_alias']]) }}">Перейти</a>
+    <a href="{{ route($testRouteName, ['subject' => $test->subject->uri_alias, 'test' => $test->uri_alias]) }}">Перейти</a>
     <span class="badge badge-primary badge-pill">{{ $test['questions_count'] }}</span>
 </li>
--- resources/views/pages/admin/subjects-single.blade.php
+++ resources/views/pages/admin/subjects-single.blade.php
@@ -8,28 +8,12 @@
 @section('subject-name') oop @endsection
 
 @section('test-links')
-    {{--  traverse given array and print all entities  --}}
-
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <h3 class="category-header">Інкапсуляція</h3>
-
-        <a href="{{ url()->current() }}/encapsulation">Перейти</a>
-        <span class="badge badge-primary badge-pill">50</span>
-    </li>
-
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <h3 class="category-header">Наслідування</h3>
-
-        <a href="#">Перейти</a>
-        <span class="badge badge-primary badge-pill">30</span>
-    </li>
-
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <h3 class="category-header">Поліморфізм</h3>
-
-        <a href="#">Перейти</a>
-        <span class="badge badge-primary badge-pill">40</span>
-    </li>
+    {{-- todo traverse given array and print all entities  --}}
+    @forelse($tests as $test)
+        @include('blocks.admin.test-line')
+    @empty
+        <h3 class="list-group-item">Тестів поки що немає. Можете створити новий натиснувши кнопку нижче:</h3>
+    @endforelse
 @endsection
 
 @section('create-new-link')
