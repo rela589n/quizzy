2e524782a5b0ad409f92a9c838a0307cc34cd0e1
rela589n <zsidelnik@gmail.com>
2020-01-31 07:49:32 +0200

    Removed mock implementation of list

--- resources/views/pages/admin/subjects-list.blade.php
+++ resources/views/pages/admin/subjects-list.blade.php
@@ -5,13 +5,6 @@
 @endsection
 
 @section('subject-links')
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <h3 class="category-header">ООП</h3>
-
-        <a href="{{ route('admin.tests.subject', ['subject' => 'oop']) }}">Перейти</a>
-        <span class="badge badge-primary badge-pill">4</span>
-    </li>
-
     @foreach($subjects as $subject)
         @include('blocks.admin.subject-line', ['subject' => $subject])
     @endforeach
