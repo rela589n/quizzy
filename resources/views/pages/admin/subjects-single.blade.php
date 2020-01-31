7337768d2a77bac97e5ca7fa2a8d6224b10f5bf6
rela589n <zsidelnik@gmail.com>
2020-01-31 09:32:37 +0200

    Mock text replaced with implementation. Url-generation using route() helper

--- resources/views/pages/admin/subjects-single.blade.php
+++ resources/views/pages/admin/subjects-single.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.subjects-single', ['baseLayout' => 'layouts.root.admin'])
 
 @section('settings-link')
-    <a class="btn btn-outline-dark finish-test-btn mt-1 float-right" href="{{ url()->current() }}/settings">Перейти до
-        налаштувань предмета</a>
+    <a class="btn btn-outline-dark finish-test-btn mt-1 float-right"
+       href="{{ route('admin.tests.subject.settings', ['subject' => $subject->uri_alias]) }}">
+        Перейти до налаштувань предмета
+    </a>
 @endsection
 
-@section('subject-name') oop @endsection
+@section('subject-name') {{ $subject->name }} @endsection
 
 @section('test-links')
-    {{-- todo traverse given array and print all entities  --}}
-    @forelse($tests as $test)
+    @forelse($subject->tests as $test)
         @include('blocks.admin.test-line')
     @empty
         <h3 class="list-group-item">Тестів поки що немає. Можете створити новий натиснувши кнопку нижче:</h3>
@@ -17,5 +18,6 @@
 @endsection
 
 @section('create-new-link')
-    <a class="btn btn-primary finish-test-btn mt-4 btn-block" href="{{ url()->current() }}/new">Новий</a>
+    <a class="btn btn-primary finish-test-btn mt-4 btn-block"
+       href="{{ route('admin.tests.subject.new', ['subject' => $subject->uri_alias]) }}">Новий</a>
 @endsection
