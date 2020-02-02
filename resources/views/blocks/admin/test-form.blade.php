7a614c68d4dab277222bcfca530a66e658162ce0
rela589n <zsidelnik@gmail.com>
2020-02-02 22:00:04 +0200

    New. Create and update test form.

--- resources/views/blocks/admin/test-form.blade.php
+++ resources/views/blocks/admin/test-form.blade.php
@@ -5,7 +5,11 @@
     </label>
     <input id="name" name="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Назва"
            required="required" value="{{  old('name',  $test->name ?? '') }}">
-
+    @error('name')
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
 
     <label for="alias" class="form-info mb-4 h3">
         Введіть псевдонім для використання в uri:
@@ -44,45 +48,51 @@
     </div>
     @enderror
 
-    <label for="include" class="form-info mb-2 h3">
-        Включити питання з наступних тестів:
-    </label>
-    <div class="form-row align-items-center flex-nowrap justify-content-start" data-variant="3">
-        <div class="col-auto">
-            <div class="form-check ">
-                <div class="d-inline-block"></div>
-                <input class="form-check-input mt-2 is-correct required-target" type="checkbox" id="t[123][include]"
-                       data-required="t[123][count]">
-            </div>
-        </div>
-        <div class="col-form-label">
-            <label for="t[123][include]" class="form-check-label variant-text">
-                Об'єктно орієнтоване програмування
-            </label>
-        </div>
-        <div class="col-2">
-            <input id="t[123][count]" type="number" class="form-control form-control-sm" placeholder="25"
-                   min="1" max="120">
-        </div>
-    </div>
+    @if(isset($includeTests) && count($includeTests) > 0)
+        <label for="include" class="form-info mb-2 h3">
+            Включити питання з наступних тестів:
+        </label>
 
-    {{--    <div class="form-row align-items-center flex-nowrap justify-content-start" data-variant="3">--}}
-    {{--        <div class="col-auto">--}}
-    {{--            <div class="form-check ">--}}
-    {{--                <div class="d-inline-block"></div>--}}
-    {{--                <input class="form-check-input mt-2 is-correct" type="checkbox" id="t[124][include]" name="t[124][include]">--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-form-label">--}}
-    {{--            <label for="t[124][include]" class="form-check-label variant-text">--}}
-    {{--                Алгоритми--}}
-    {{--            </label>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-2">--}}
-    {{--            <input id="t[124][count]" name="t[124][count]" type="number" class="form-control form-control-sm"--}}
-    {{--                   placeholder="25" required="required" min="1" max="120">--}}
-    {{--        </div>--}}
-    {{--    </div>--}}
+        @forelse($includeTests as $test)
+            <div class="form-row align-items-center flex-nowrap justify-content-start">
+                <div class="col-auto">
+                    <div class="form-check ">
+                        <div class="d-inline-block"></div>
+                        <input id="include[{{ $test->id }}][necessary]"
+                               name="include[{{ $test->id }}][necessary]"
+                               type="checkbox"
+                               class="form-check-input mt-2 is-correct required-target"
+                               data-required="include[{{ $test->id }}][count]"
+                               @if(old("include.{$test->id}.necessary", false)) checked="checked" @endif>
+                    </div>
+                </div>
+                <div class="col-form-label">
+                    <label for="include[{{ $test->id }}][necessary]" class="form-check-label variant-text">
+                        {{ $test->name }}
+                    </label>
+                </div>
+
+                <div class="@error("include.{$test->id}.count") col-auto @else col-2  @enderror">
+                    <input id="include[{{ $test->id }}][count]"
+                           name="include[{{ $test->id }}][count]"
+                           type="number"
+                           class="form-control form-control-sm @error("include.{$test->id}.count") is-invalid @enderror"
+                           placeholder="{{ $test->questions_count }}"
+                           min="1" max="{{ $test->questions_count }}"
+                           value="{{ old("include.{$test->id}.count", '') }}"
+                           @if(old("include.{$test->id}.necessary", false)) required="required" @endif {{-- if checkbox checked then it is required--}}
+                    >
+                    @error("include.{$test->id}.count")
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+            </div>
+        @empty
+            <h3 class="list-group-item">Поки що немає тестів, з яких можна було б включити питання</h3>
+        @endforelse
+    @endif
 
     <button type="submit"
             class="btn btn-primary btn-block finish-test-btn mb-4 mt-4">{{ $submitButtonText ?? 'Зберегти' }}</button>
