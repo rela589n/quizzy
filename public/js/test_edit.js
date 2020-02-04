adc4b85394dc17278b39875e93a991d5111b51aa
rela589n <zsidelnik@gmail.com>
2020-02-04 16:26:30 +0200

    Bug. Crazy not adequate refactoring

--- public/js/test_edit.js
+++ public/js/test_edit.js
@@ -4,6 +4,54 @@ $(function () {
     let $questionsContainer = $editTestForm.find('ul.list-group.questions');
     let creator = new QuestionCreator($questionsContainer);
 
+    function QuestionRecord(html) {
+        let $question = $(html);
+        let variants = [];
+
+        this.changeIndex = function (newIndex) {
+            $question.attr('data-question', newIndex);
+            $question.children('.question-header').text(creator.questionTextPlaceholder(newIndex));
+        };
+
+        this.pushVariant = function (variantRecord) {
+            variants.push(variantRecord);
+        };
+
+        this.getVariant = function (index) {
+            return variants[index];
+        };
+
+        this.callNextVariantUpdate = function(nextIndex) {
+
+        }
+
+        this.getResultHtml = function () {
+            let $wrapper = $question.find('.variants-wrapper');
+            for (let i = 0; i < variants.length; ++i) {
+                $wrapper.append(variants[i].getResultHtml());
+            }
+
+            return $question;
+        }
+    }
+
+    function VariantRecord(html, questionRecord) {
+        let $variant = $(html);
+        let parent = questionRecord;
+
+
+        this.changeIndex = function ($variant, index) {
+            $variant.attr('data-variant', index);
+            $variant.find('.variant-text').attr('placeholder', creator.variantTextPlaceholder(index));
+            $variant.find('.is-correct').attr('name', creator.generateCheckBoxName(parent.))
+            // $('.is-correct', $variant).attr('name', creator.)
+        };
+
+        this.getResultHtml = function () {
+            return $variant;
+        };
+    }
+
     function QuestionCreator($questionsContainer) {
         this.$questionsContainer = $questionsContainer;
 
@@ -11,8 +59,8 @@ $(function () {
             let newQuestionIndex = (+this.$questionsContainer.children('li:last').attr('data-question') + 1) || 1;
             let $newQuestion = this.createEmptyQuestion(newQuestionIndex);
 
-            let $variant1 = this.createEmptyVariant(1);
-            let $variant2 = this.createEmptyVariant(2);
+            let $variant1 = this.createEmptyVariant(1, newQuestionIndex);
+            let $variant2 = this.createEmptyVariant(2, newQuestionIndex);
 
             this.pushVariantToQuestion($newQuestion, $variant1);
             this.pushVariantToQuestion($newQuestion, $variant2);
@@ -26,41 +74,70 @@ $(function () {
 
         this.createEmptyQuestion = function (questionIndex) {
             return $(`<li class="list-group-item mb-4 question" data-question="${questionIndex}" data-new="true">
-                    <h4 class="text-center mb-3 question-header">Питання № ${questionIndex}</h4>
-                    
+                    <label for="q[new][${questionIndex}][name]"
+                           class="text-center mb-3 h4 d-block question-header">
+                           ${this.questionTextPlaceholder(questionIndex)}
+                    </label>
+
                     <button type="button" class="btn btn-danger position-absolute button-delete-question" tabindex="-1"><i class="fas fa-trash"></i></button>
 
-                    <input type="text" class="form-control" placeholder="Запитання" required="required">
+                    <input type="text"
+                           id="q[new][${questionIndex}][name]"
+                           name="q[new][${questionIndex}][name]"
+                           class="form-control question-text"
+                           placeholder="Запитання"
+                           required="required">
                     <div class="variants-wrapper">
-                        
+
                     </div>
-                    
+
                     <button type="button" class="btn btn-outline-primary btn-sm button-add-variant mt-2"><i
                                 class="fas fa-plus"></i></button>
                 </li>`);
         };
 
-        this.createEmptyVariant = function (variantIndex) {
-            let newCheckboxId = 'id_' + this.generateId();
-            let newVariantId = 'id_' + this.generateId();
+        this.createEmptyVariant = function (variantIndex, questionIndex) {
 
             return $(`<div class="form-row align-items-center" data-variant="${variantIndex}">
                     <div class="col-auto">
-                        <div class="form-check mb-2">
-                            <label class="form-check-label" for="${newCheckboxId}"></label>
-                            <input class="form-check-input is-correct" type="checkbox" id="${newCheckboxId}">
-                        </div>
+                        <label class="form-check d-inline pb-1 mb-0">
+                            <input class="form-check-input is-correct" type="checkbox"
+                                   name="${this.generateCheckBoxName(variantIndex, questionIndex)}">
+                        </label>
                     </div>
                     <div class="col-form-label col-xl-11 col-lg-10 col-sm-9 col-8">
-                        <label class="form-check-label d-block" for="${newVariantId}">
-                            <input type="text" class="form-control form-control-sm variant-text" id="${newVariantId}"
-                                   placeholder="Варіант № ${variantIndex}"  required="required">
+                        <label class="form-check-label d-block">
+                            <input type="text" class="form-control form-control-sm variant-text"
+                                   name="${this.generateVariantInputName(variantIndex, questionIndex)}"
+                                   placeholder="${this.variantTextPlaceholder(variantIndex)}"  required="required">
                         </label>
                     </div>
                     <div class="col-auto">
                         <button type="button" class="btn btn-outline-danger btn-sm button-delete-variant" tabindex="-1"><i class="fas fa-trash"></i></button>
                     </div>
                 </div>`);
+        };
+
+        this.changeVariantIndex = function ($variant, index) {
+            $variant.attr('data-variant', index);
+            $variant.find('.variant-text').attr('placeholder', 'Варіант № ' + index);
+            // $('.is-correct', $variant).attr('name', creator.)
+        };
+
+        this.variantTextPlaceholder = function (variantIndex) {
+            return `Варіант № ${variantIndex}`;
+        };
+
+        this.questionTextPlaceholder = function (questionIndex) {
+            return 'Питання № ' + questionIndex;
+        };
+
+        this.generateCheckBoxName = function (variantIndex, questionIndex) {
+            return `q[new][${questionIndex}][v][${variantIndex}][is_right]`;
+        };
+
+        this.generateVariantInputName = function (variantIndex, questionIndex) {
+            return `q[new][${questionIndex}][v][${variantIndex}][text]`;
         };
 
         this.generateId = function () {
@@ -80,10 +157,13 @@ $(function () {
 
     $document.on('click', '.button-add-variant', function () {
         let $this = $(this);
+        let $question = $this.closest('li.question');
         let $variantsWrapper = $this.siblings('.variants-wrapper');
-        let newVariantIndex = +$variantsWrapper.children('div:last').attr('data-variant') + 1 || 1;
 
-        $variantsWrapper.append(creator.createEmptyVariant(newVariantIndex));
+        let newVariantIndex = +$variantsWrapper.children('div:last').attr('data-variant') + 1 || 1;
+        let questionIndex = $question.attr('data-question');
+
+        $variantsWrapper.append(creator.createEmptyVariant(newVariantIndex, questionIndex));
     });
 
     $document.on('click', '.button-delete-variant', function () {
@@ -101,7 +181,8 @@ $(function () {
 
         function changeVariantIndex($variant, index) {
             $variant.attr('data-variant', index);
-            $variant.find('.variant-text').attr('placeholder', 'Варіант ' + index);
+            $variant.find('.variant-text').attr('placeholder', 'Варіант № ' + index);
+            // $('.is-correct', $variant).attr('name', creator.)
         }
 
     });
