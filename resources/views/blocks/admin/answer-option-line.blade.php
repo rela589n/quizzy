7a6b2a6a353961f7eebe185b7d0157d5a20829f6
rela589n <zsidelnik@gmail.com>
2020-02-08 11:33:31 +0200

    New. Displaying validation errors.

--- resources/views/blocks/admin/answer-option-line.blade.php
+++ resources/views/blocks/admin/answer-option-line.blade.php
@@ -4,17 +4,23 @@
             <input class="form-check-input is-correct"
                    type="checkbox"
                    name="q[{{ $type }}][{{ $question->id }}][v][{{ $option->id }}][is_right]"
-                   @if( old("q.{$type}.{$question->id}.v.{$option->id}.is_right", $option->is_right ?? false)) checked="checked" @endif
+                   @if( $option->is_right ?? false) checked="checked" @endif
             >
         </label>
     </div>
     <div class="col-form-label col-xl-11 col-lg-10 col-sm-9 col-8">
         <label class="form-check-label d-block">
-            <input type="text" class="form-control form-control-sm variant-text"
+            <input type="text" class="form-control form-control-sm variant-text @error("q.{$type}.{$question->id}.v.{$option->id}.text") is-invalid @enderror"
                    name="q[{{ $type }}][{{ $question->id }}][v][{{ $option->id }}][text]"
                    placeholder="Варіант № {{ $optionIndex }}"
-                   value="{{ old("q.{$type}.{$question->id}.v.{$option->id}.text", $option->text ?? '') }}"
+                   value="{{ $option->text }}"
                    required="required">
+
+            @error("q.{$type}.{$question->id}.v.{$option->id}.text")
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </label>
     </div>
 
--- resources/views/blocks/admin/question-single.blade.php
+++ resources/views/blocks/admin/question-single.blade.php
@@ -16,16 +16,29 @@
     <input type="text"
            id="q[{{ $type }}][{{ $question->id }}][name]"
            name="q[{{ $type }}][{{ $question->id }}][name]"
-           class="form-control question-text" placeholder="Запитання"
-           value="{{ old("q.{$type}.{$question->id}.name", $question->question ?? '') }}" required="required">
+           class="form-control question-text @error("q.$type.{$question->id}.name") is-invalid @enderror"
+           placeholder="Запитання"
+           value="{{ $question->question }}" required="required">
 
-    <div class="variants-wrapper">
+    @error("q.$type.{$question->id}.name")
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
+
+    <div class="variants-wrapper @error("q.$type.{$question->id}.v") is-invalid @enderror">
         @foreach($question->answerOptions as $option)
             @include('blocks.admin.answer-option-line', [
               'optionIndex' => $loop->iteration,
             ])
         @endforeach
     </div>
+    @error("q.$type.{$question->id}.v")
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
+
 
     <button type="button" class="btn btn-outline-primary btn-sm button-add-variant mt-2"><i
             class="fas fa-plus"></i></button>
