e397d121b5bd58f636d86289e31e117f643761d4
rela589n <zsidelnik@gmail.com>
2020-04-25 11:30:46 +0300

    Enhancement. Seed answer options for first two questions without lorem ipsum.

--- database/seeds/AnswerOptionsTableSeeder.php
+++ database/seeds/AnswerOptionsTableSeeder.php
@@ -16,41 +16,55 @@ class AnswerOptionsTableSeeder extends Seeder
      */
     public function run()
     {
-        AnswerOption::create([
-            'text' => 'це спеціальна конструкція, яка використовується для групування пов\'язаних змінних та функцій.',
-            'question_id' => 1,
-            'is_right' => true
-        ]);
-
-        AnswerOption::create([
-            'text' => 'це функція, що дає змогу отримати значення глобальних змінних.',
-            'question_id' => 1,
-            'is_right' => false
-        ]);
-
-        AnswerOption::create([
-            'text' => 'це макет, по якому можна створювати об\'єкти.',
-            'question_id' => 1,
-            'is_right' => true
-        ]);
-
         $faker = Faker\Factory::create('uk_UA');
 
         $questions = Question::all();
         foreach ($questions as $question) {
             AnswerOption::create([
-                'text' => $faker->realText(40),
+                'text'        => $faker->realText(40),
                 'question_id' => $question->id,
-                'is_right' => true
+                'is_right'    => true
             ]);
 
-            foreach(range(2, $faker->numberBetween(self::PER_QUESTION_MIN, self::PER_QUESTION_MAX)) as $i) {
+            foreach (range(2, $faker->numberBetween(self::PER_QUESTION_MIN, self::PER_QUESTION_MAX)) as $i) {
                 AnswerOption::create([
-                    'text' => $faker->realText(40),
+                    'text'        => $faker->realText(40),
                     'question_id' => $question->id,
-                    'is_right' => $faker->boolean()
+                    'is_right'    => $faker->boolean()
                 ]);
             }
         }
+
+        $question = Question::find(1);
+        $question->answerOptions()->delete();
+
+        $question->answerOptions()->createMany([
+            [
+                'text'     => 'це спеціальна конструкція, яка використовується для групування пов\'язаних змінних та функцій.',
+                'is_right' => true
+            ],
+            [
+                'text'     => 'це функція, що дає змогу отримати значення глобальних змінних.',
+                'is_right' => false
+            ],
+            [
+                'text'     => 'це макет, по якому можна створювати об\'єкти.',
+                'is_right' => true
+            ]
+        ]);
+
+        $question = Question::find(2);
+        $question->answerOptions()->delete();
+
+        $question->answerOptions()->createMany([
+            [
+                'text'     => 'Лише 1',
+                'is_right' => false
+            ],
+            [
+                'text'     => 'Скільки потрібно',
+                'is_right' => true
+            ]
+        ]);
     }
 }
