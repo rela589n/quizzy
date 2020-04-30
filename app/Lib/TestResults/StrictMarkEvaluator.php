94fa51cc9f2b3946ea26961f4676b5671e899265
rela589n <zsidelnik@gmail.com>
2020-04-30 13:29:26 +0300

    Ehnancement. Correlate string marks evaluating.

--- app/Lib/TestResults/StrictMarkEvaluator.php
+++ app/Lib/TestResults/StrictMarkEvaluator.php
@@ -11,6 +11,21 @@ class StrictMarkEvaluator implements MarkEvaluatorInterface
      */
     public function putMark(float $fullTestScore): int
     {
-        return max(1, (int)round($fullTestScore * 5 - 0.1));
+    	$fullTestScore *= 100;
+
+    	if ($fullTestScore < 60){
+    		return 2;
+    	}
+
+    	if ($fullTestScore < 75) {
+    		return 3;
+    	}
+
+    	if ($fullTestScore < 95) {
+    		return 4;
+    	}
+
+    	return 5;
+        //return max(1, (int)round($fullTestScore * 5 - 0.1));
     }
 }
