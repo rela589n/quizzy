b970a0bdd36a46de7031d82c00d114c321a4b332
rela589n <zsidelnik@gmail.com>
2020-05-01 15:43:56 +0300

    New. Validate uploaded file.

---
+++ app/Http/Requests/Tests/ImportTestRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Tests;
+
+use App\Http\Requests\RequestUrlManager;
+use App\Models\Administrator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImportTestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @param Administrator $user
+     * @param RequestUrlManager $urlManager
+     * @return bool
+     */
+    public function authorize(Administrator $user, RequestUrlManager $urlManager)
+    {
+        return $user->can('update', $urlManager->getCurrentTest());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'selected-file' => [
+                'required',
+                'file',
+                'max:2048',
+                'mimes:docx'
+            ]
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'selected-file' => '"Вибраний файл"'
+        ];
+    }
+}
