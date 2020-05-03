073384e6d5172d0fbe5f7fe211d31f71ae1268fb
rela589n <zsidelnik@gmail.com>
2020-05-03 20:49:10 +0300

    Enhancement. Responsive layout of delete-entity button.

--- resources/views/blocks/admin/delete-entity-form.blade.php
+++ resources/views/blocks/admin/delete-entity-form.blade.php
@@ -3,7 +3,7 @@
         @method('delete')
         @csrf
         <button type="submit"
-                class="btn btn-outline-danger finish-test-btn col-2 float-lg-right float-md-none @error('delete') is-invalid @enderror"
+                class="btn btn-outline-danger finish-test-btn col-4 col-sm-3 col-md-2 float-right @error('delete') is-invalid @enderror"
                 style="margin-top: -4.13rem;">{{ $deleteText ?? 'Видалити' }}
         </button>
 
--- resources/views/blocks/admin/submit-button.blade.php
+++ resources/views/blocks/admin/submit-button.blade.php
@@ -1,5 +1,11 @@
 <div class="form-row justify-content-between">
-    <div class="col-{{ $columns ?? 9 }}">
+    @php
+        $columnsMd = $columns ?? 9;
+        $columnsSm = ($columnsMd !== 12) ? $columnsMd - 1 : 12;
+        $columnsXs = ($columnsMd !== 12) ? $columnsSm - 1 : 12;
+    @endphp
+
+    <div class="col-{{ $columnsXs }} col-sm-{{ $columnsSm }} col-md-{{ $columnsMd }}">
         <button type="submit"
                 class="btn btn-primary btn-block finish-test-btn mb-4 mt-3">{{ $slot }}</button>
     </div>
