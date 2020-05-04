add9783cc614bd2afddf9566f62937a67ecb3e24
rela589n <zsidelnik@gmail.com>
2020-05-04 15:42:58 +0300

    Refactoring. Move common Uri-alias field into separate file.

---
+++ resources/views/blocks/admin/uri-alias-field.blade.php
@@ -0,0 +1,24 @@
+@section('alias-label')
+    <label for="alias" class="form-info mb-4 h3">
+        {{ $aliasText ?? 'Введіть псевдонім для використання в uri:' }}
+    </label>
+@show
+
+<div class="form-group form-row align-items-start">
+    <div class="col-9">
+        <input id="alias" name="uri_alias" type="text" class="form-control @error('uri_alias') is-invalid @enderror"
+               placeholder="Псевдонім"
+               required="required" value="{{ old('uri_alias', $test->uri_alias ?? '') }}">
+
+        @error('uri_alias')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+    <div class="col-3">
+        <button tabindex="-1" type="button" id="auto-generate-translit" class="btn btn-primary btn-block"
+                disabled="disabled">Автоматично
+        </button>
+    </div>
+</div>
