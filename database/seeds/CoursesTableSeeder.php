0c2c593bea61cb0026c0c8ca35cb61f0b208c596
rela589n <zsidelnik@gmail.com>
2020-05-06 15:17:01 +0300

    New. Seed courses for production and sync existing subjects with courses.

--- database/seeds/CoursesTableSeeder.php
+++ database/seeds/CoursesTableSeeder.php
@@ -1,26 +1,27 @@
 <?php
 
 use App\Models\Course;
+use App\Models\TestSubject;
 use Illuminate\Database\Seeder;
 
 class CoursesTableSeeder extends Seeder
 {
     protected static $courses = [
         [
-            'id' => 1,
-            'public_name'  => 'перший',
+            'id'          => 1,
+            'public_name' => 'перший',
         ],
         [
-            'id' => 2,
-            'public_name'  => 'другий',
+            'id'          => 2,
+            'public_name' => 'другий',
         ],
         [
-            'id' => 3,
-            'public_name'  => 'третій',
+            'id'          => 3,
+            'public_name' => 'третій',
         ],
         [
-            'id' => 4,
-            'public_name'  => 'четвертий',
+            'id'          => 4,
+            'public_name' => 'четвертий',
         ],
     ];
 
@@ -32,5 +33,16 @@ class CoursesTableSeeder extends Seeder
     public function run()
     {
         Course::insert(self::$courses);
+
+        if (env('APP_ENV') === 'production') {
+            /**
+             * @var TestSubject[] $subjects
+             */
+            $subjects = TestSubject::all();
+
+            foreach ($subjects as $subject) {
+                $subject->courses()->sync([$subject->course], false);
+            }
+        }
     }
 }
