642ba749873acd74a56e6452d4f44d56359b5d3c
rela589n <zsidelnik@gmail.com>
2020-05-12 22:18:05 +0300

    New. Select for class monitor.

--- resources/views/blocks/admin/student-group-form.blade.php
+++ resources/views/blocks/admin/student-group-form.blade.php
@@ -22,8 +22,8 @@
         $currentYear = date('Y');
         $groupEnter = old('year', $group['year'] ?? $currentYear);
     @endphp
-    <select class="browser-default custom-select mb-2 @error('year') is-invalid @enderror" required="required" id="year"
-            name="year">
+    <select class="browser-default custom-select mb-2 @error('year') is-invalid @enderror" required="required"
+            id="year" name="year">
         @for ($i = min($groupEnter, $currentYear - 4); $i <= $currentYear; ++$i)
             <option value="{{ $i }}" @if($i === $groupEnter) selected="selected" @endif>{{ $i }}</option>
         @endfor
@@ -34,6 +34,29 @@
         {{ $message }}
     </div>
     @enderror
+
+    @if(count($classMonitors) && $authUser->can('edit-class-monitors'))
+        <label for="created_by" class="form-info mb-4 h3">
+            Адміністратор групи (староста):
+        </label>
+        <select class="browser-default custom-select mb-2 @error('created_by') is-invalid @enderror"
+                id="created_by" name="created_by">
+            <option value="">Не обрано</option>
+
+            @foreach($classMonitors as $classMonitor)
+                <option value="{{ $classMonitor->id }}"
+                        @if($classMonitor->id === $group->created_by) selected="selected" @endif>
+                    {{ $classMonitor->full_name }}
+                </option>
+            @endforeach
+        </select>
+
+        @error('created_by')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    @endif
 
     @section('user-form-submit')
         @component('blocks.admin.submit-button', ['columns' => $submitSize ?? 12])
