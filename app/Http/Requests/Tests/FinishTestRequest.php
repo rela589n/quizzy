96c08837b27fbbd2ef5483f68bb7d284c77bdfb6
rela589n <zsidelnik@gmail.com>
2020-05-16 11:21:31 +0300

    Refactoring. Receive user from method parameter instead of calling method.

--- app/Http/Requests/Tests/FinishTestRequest.php
+++ app/Http/Requests/Tests/FinishTestRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Tests;
 
 
 use App\Http\Requests\RequestUrlManager;
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class FinishTestRequest extends FormRequest
@@ -11,12 +12,13 @@ class FinishTestRequest extends FormRequest
     /**
      * Determine if the student is authorized to pass current test.
      *
+     * @param User $user
      * @param RequestUrlManager $urlManager
      * @return bool
      */
-    public function authorize(RequestUrlManager $urlManager)
+    public function authorize(User $user, RequestUrlManager $urlManager)
     {
-        return $this->user('client')->can('pass-test', $urlManager->getCurrentTest());
+        return $user->can('pass-test', $urlManager->getCurrentTest());
     }
 
     /**
@@ -27,20 +29,24 @@ class FinishTestRequest extends FormRequest
     public function rules()
     {
         return [
-            'ans' => 'array',
-            'ans.*' => 'array',
+            'ans'     => 'array',
+            'ans.*'   => 'array',
             'ans.*.*' => 'array',
+
             'ans.*.*.answer_option_id' => [
                 'required',
                 'numeric'
             ],
+
             'ans.*.*.is_chosen' => [
                 'sometimes',
                 'required',
                 'numeric'
             ],
-            'asked' => 'array|required',
+
+            'asked'   => 'array|required',
             'asked.*' => 'array',
+
             'asked.*.question_id' => [
                 'required',
                 'numeric'
