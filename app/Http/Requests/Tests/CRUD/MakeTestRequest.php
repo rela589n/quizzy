0513e47e71f22293e144f4b3133befa250c78c13
rela589n <zsidelnik@gmail.com>
2020-05-16 10:30:11 +0300

    Refactoring. Move validation messages into language files.

--- app/Http/Requests/Tests/CRUD/MakeTestRequest.php
+++ app/Http/Requests/Tests/CRUD/MakeTestRequest.php
@@ -23,30 +23,6 @@ abstract class MakeTestRequest extends FormRequest
         $this->baseRules['uri_alias'][] = new UriSlug();
     }
 
-    /**
-     * Get custom attributes for validator errors.
-     *
-     * @return array
-     */
-    public function attributes()
-    {
-        return [ // something wrong is going with substitution of attribute names in arrays
-            'include.*.count' => '"Кількість"'
-        ];
-    }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            'include.*.count.required_with' => 'Якщо ви вибрали цей предмет, то обов\'язково вкажіть кількість питань з нього.'
-        ];
-    }
-
     protected $baseRules = [
         'name'            => [
             'required',
