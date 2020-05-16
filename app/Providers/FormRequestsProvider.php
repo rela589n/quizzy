01a2cd273329ed1d56e0ebf505bd74a642cc6197
rela589n <zsidelnik@gmail.com>
2020-05-16 10:50:10 +0300

    Refactoring. Remove UrlManageable spike-nail because of no-use.

--- app/Providers/FormRequestsProvider.php
+++ app/Providers/FormRequestsProvider.php
@@ -5,8 +5,6 @@ namespace App\Providers;
 use App\Http\Requests\Auth\AdminChangePasswordRequest;
 use App\Http\Requests\Auth\StudentChangePasswordRequest;
 use App\Http\Requests\Questions\FillAnswersRequest;
-use App\Http\Requests\RequestUrlManager;
-use App\Http\Requests\UrlManageable;
 use App\Lib\ValidationGenerator;
 use App\Models\Administrator;
 use App\Models\User;
@@ -15,9 +13,6 @@ use Illuminate\Support\ServiceProvider;
 
 class FormRequestsProvider extends ServiceProvider
 {
-    protected $urlManageable = [
-    ];
-
     /**
      * Register services.
      *
@@ -39,12 +34,5 @@ class FormRequestsProvider extends ServiceProvider
             $service->setAuthUser($app->make(User::class));
             return $service;
         });
-
-        foreach ($this->urlManageable as $request) {
-            $this->app->extend($request, function (UrlManageable $service, Application $app) {
-                $service->setUrlManager($app->make(RequestUrlManager::class));
-                return $service;
-            });
-        }
     }
 }
