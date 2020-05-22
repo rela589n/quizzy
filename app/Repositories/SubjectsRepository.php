b0f24ac3ac97be7b47f358ec14acd6b5fdb2a6a9
rela589n <zsidelnik@gmail.com>
2020-05-22 13:40:16 +0300

    Refactoring. Type-hint variable.

--- app/Repositories/SubjectsRepository.php
+++ app/Repositories/SubjectsRepository.php
@@ -5,12 +5,13 @@ namespace App\Repositories;
 
 
 use App\Models\TestSubject;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class SubjectsRepository
 {
     public function subjectsToInclude()
     {
-        return TestSubject::with(['tests' => function ($query) {
+        return TestSubject::with(['tests' => function (Relation $query) {
             $query->has('nativeQuestions')
                 ->withCount('nativeQuestions as questions_count');
         }])->get();
