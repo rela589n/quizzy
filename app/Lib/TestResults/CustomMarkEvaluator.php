3efb3ae117bf41a92928d2287a3cd4acfdcc5e8c
rela589n <zsidelnik@gmail.com>
2020-05-28 22:26:54 +0300

    New. Custom user-defined mark evaluator.

--- app/Lib/TestResults/CustomMarkEvaluator.php
+++ app/Lib/TestResults/CustomMarkEvaluator.php
@@ -4,13 +4,70 @@
 namespace App\Lib\TestResults;
 
 
+use App\Exceptions\NullPointerException;
+use App\Models\Test;
+
 class CustomMarkEvaluator implements MarkEvaluator
 {
+    protected const UNBOUND_MARK = 2;
+
+    /**
+     * @var Test
+     */
+    protected $test;
+
+    /**
+     * @var array
+     */
+    protected $markPercentsMap;
+
+    /**
+     * @param Test $test
+     * @return CustomMarkEvaluator
+     */
+    public function setTest(Test $test): self
+    {
+        if ($this->test !== $test) {
+
+            $this->markPercentsMap = $this->markPercentsMapCreator
+                ->setModels($test->marksPercents)
+                ->getMap();
+
+            $this->test = $test;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @var MarkPercentsMapCreator
+     */
+    private $markPercentsMapCreator;
+
+    public function __construct(MarkPercentsMapCreator $markPercentsMap)
+    {
+        $this->markPercentsMapCreator = $markPercentsMap;
+    }
+
     /**
      * @inheritDoc
+     * @throws NullPointerException
      */
     public function putMark(float $fullTestScore): int
     {
-        // TODO: Implement putMark() method.
+        $fullTestScore *= 100;
+
+        if ($this->test === null) {
+            throw new NullPointerException('To evaluate test mark, respectively need test to be set.');
+        }
+
+        foreach ($this->markPercentsMap as $mark => $percents) {
+
+            if ($fullTestScore >= $percents) {
+                return $mark;
+            }
+        }
+
+        return self::UNBOUND_MARK;
     }
 }
