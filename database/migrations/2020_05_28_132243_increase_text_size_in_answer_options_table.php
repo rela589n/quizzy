29331dcbf1d6585d066cc63a82c05b73f2db73ed
rela589n <zsidelnik@gmail.com>
2020-05-28 13:26:17 +0300

    Enhancement. Increase text size in anser_options table.

---
+++ database/migrations/2020_05_28_132243_increase_text_size_in_answer_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class IncreaseTextSizeInAnswerOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('answer_options', function (Blueprint $table) {
+            $table->string('text', 255)
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answer_options', function (Blueprint $table) {
+            $table->string('text', 128)
+                ->change();
+        });
+    }
+}
