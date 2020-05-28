6ebd2180460847a5092f306568ab019ec9eff240
rela589n <zsidelnik@gmail.com>
2020-05-28 22:28:11 +0300

    New. Factory for mark evaluators.

---
+++ app/Factories/MarkEvaluatorsFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\Factories;
+
+
+use App\Exceptions\UnknownMarkEvaluatorTypeException;
+use App\Lib\TestResults\CustomMarkEvaluator;
+use App\Lib\TestResults\StrictMarkEvaluator;
+use App\Models\Test;
+use Illuminate\Foundation\Application;
+
+class MarkEvaluatorsFactory
+{
+    /**
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    private $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @var Test
+     */
+    private $test;
+
+    /**
+     * @param Test $test
+     * @return MarkEvaluatorsFactory
+     */
+    public function setTest(Test $test): self
+    {
+        $this->test = $test;
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $type
+     * @return CustomMarkEvaluator|StrictMarkEvaluator|mixed
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function resolve(string $type = null)
+    {
+        if ($type === null) {
+            $type = $this->test->mark_evaluator_type;
+        }
+
+        switch ($type) {
+            case 'default':
+                return $this->app->make(StrictMarkEvaluator::class);
+
+            case 'custom':
+                $markEvaluator = $this->app->make(CustomMarkEvaluator::class);
+                $markEvaluator->setTest($this->test);
+
+                return $markEvaluator;
+
+            default:
+                throw new UnknownMarkEvaluatorTypeException('Could not find mark evaluator for "' . $type . '" type.');
+        }
+    }
+}
