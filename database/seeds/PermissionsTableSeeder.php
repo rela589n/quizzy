b27bc21eda5aef2f7d016817f64f0f09d7b547bf
rela589n <zsidelnik@gmail.com>
2020-05-30 23:07:12 +0300

    New. edit-group-of-student permission.

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -110,7 +110,10 @@ class PermissionsTableSeeder extends Seeder
             ],
             'edit-class-monitors'        => [
                 'public_name' => 'Оновлення старости групи'
-            ]
+            ],
+            'edit-group-of-student'         => [
+                'public_name' => 'Змінювати групу студента'
+            ],
         ]
     ];
 
