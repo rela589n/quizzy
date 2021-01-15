8422a7a2f665332a2e1768bc07ba5c3e35338f7c
rela589n <zsidelnik@gmail.com>
2021-01-15 07:09:47 +0200

    feat(nova/actions): AttachTestsQuestionsToTest

---
+++ app/Nova/Actions/AttachTestsQuestionsToTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Models\Test;
+use App\Models\TestSubject;
+use Epartment\NovaDependencyContainer\NovaDependencyContainer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Select;
+
+class AttachTestsQuestionsToTest extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        // todo
+        dump($fields);
+        dd($models);
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        // todo move into repository because of large amount of queries
+        // todo maybe filter by departments of teacher and available tests
+        $subjects = TestSubject::query()
+            ->with('tests')
+            ->get();
+
+        return [
+            $this->createTestSubjectField($subjects),
+
+            ...$this->createTestDependentFields($subjects),
+        ];
+    }
+
+    private function createTestSubjectField(EloquentCollection $subjects): Select
+    {
+        return Select::make('Test Subject', 'test_subject')
+            ->options(
+                $subjects->mapWithKeys(
+                    fn(TestSubject $subject) => [$subject->id => "({$subject->id}) {$subject->name}"]
+                )
+            )
+            ->displayUsingLabels()
+            ->rules('required');
+    }
+
+    private function createTestDependentFields(EloquentCollection $subjects)
+    {
+        $testsBySubjects = $subjects->mapWithKeys(fn(TestSubject $subject) => [$subject->id => $subject->tests]);
+
+        return $testsBySubjects->map(
+            fn(Collection $tests, int $subjectId) => NovaDependencyContainer::make(
+                [
+                    Select::make('Test', 'test')
+                        ->options(
+                            $tests->mapWithKeys(
+                                fn(Test $test) => [$test->id => "({$test->id}) {$test->name}"]
+                            )
+                        )
+                        ->displayUsingLabels()
+                        ->rules('required'),
+                ]
+            )->dependsOn('test_subject', $subjectId)
+        );
+    }
+}
