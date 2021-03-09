c82816cd941c0dd59572ba903d2ecec3f4ac2b6f
rela589n <zsidelnik@gmail.com>
2021-03-09 07:00:01 +0200

    fix: redirect to change password form on login

--- app/Nova/Customization/LoginController.php
+++ app/Nova/Customization/LoginController.php
@@ -6,22 +6,16 @@ declare(strict_types=1);
 namespace App\Nova\Customization;
 
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Nova\Nova;
 
 final class LoginController extends \Laravel\Nova\Http\Controllers\LoginController
 {
-    /**
-     * Get the post register / login redirect path.
-     *
-     * @return string
-     */
-    public function redirectPath(): string
+    protected function authenticated(Request $request, $user)
     {
-        if (false === optional($this->guard()->user())->password_changed) {
-            return RouteServiceProvider::novaChangePasswordUrl();
+        if (false === $user->password_changed) {
+            return redirect()->to(RouteServiceProvider::novaChangePasswordUrl());
         }
-
-        return Nova::path();
     }
 }
