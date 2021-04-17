29ef9a61f35abb49b03f803f6a8f93ea5b9e88b3
rela589n <zsidelnik@gmail.com>
2021-04-17 13:48:48 +0300

    style(routes): reformat code

--- routes/client.php
+++ routes/client.php
@@ -12,22 +12,29 @@
 |
 */
 
-Route::namespace('Auth')->group(
-    static function () {
-        Route::get('/', 'LoginController@showLoginForm')->name('.login');
-        Route::post('/', 'LoginController@login');
+Route::namespace('Auth')
+    ->group(
+        static function () {
+            Route::get('/', 'LoginController@showLoginForm')
+                ->name('.login');
 
-        Route::post('/logout', 'LoginController@logout')->name('.logout');
+            Route::post('/', 'LoginController@login');
 
-        Route::get('/change-password', 'ChangePasswordController@showInitialPasswordChangeForm')->name(
-            '.change-password'
-        );
-        Route::post('/change-password', 'ChangePasswordController@initialChangePassword');
-    }
-);
+            Route::post('/logout', 'LoginController@logout')
+                ->name('.logout');
 
-Route::get('/dashboard', 'DashboardController@showDashboardPage')->name('.dashboard');
-Route::get('/documentation', 'DocumentationController@getWordDocument')->name('.documentation');
+            Route::get('/change-password', 'ChangePasswordController@showInitialPasswordChangeForm')
+                ->name('.change-password');
+
+            Route::post('/change-password', 'ChangePasswordController@initialChangePassword');
+        }
+    );
+
+Route::get('/dashboard', 'DashboardController@showDashboardPage')
+    ->name('.dashboard');
+
+Route::get('/documentation', 'DocumentationController@getWordDocument')
+    ->name('.documentation');
 
 Route::prefix('/tests')
     ->name('.tests')
