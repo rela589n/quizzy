0d73305c4eb277fa572f3745962b5293189d8999
rela589n <zsidelnik@gmail.com>
2021-05-22 20:59:24 +0300

    fix(deploy): create storage symlink

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -7,6 +7,7 @@ $composer = './composer.phar';
 shell_exec('cd ~/app');
 shell_exec("$php $composer install --no-ansi --no-progress --no-interaction");
 shell_exec("$php $composer dump-autoload");
+shell_exec("$php artisan storage:link");
 shell_exec("$php artisan nova:publish");
 shell_exec("$php artisan view:clear");
 shell_exec("$php artisan migrate");
