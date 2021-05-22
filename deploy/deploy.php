d267e3ae6468adfd9fb23ee4a67b06a51b4eb322
rela589n <zsidelnik@gmail.com>
2021-05-22 08:04:45 +0300

    deploy: add script

---
+++ deploy/deploy.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+$php = 'php7.4';
+$composer = './composer.phar';
+
+shell_exec('cd ~/app');
+shell_exec("$php $composer install --no-ansi --no-progress --no-interaction");
+shell_exec("$php $composer dump-autoload");
+shell_exec("$php artisan nova:publish");
+shell_exec("$php artisan view:clear");
+shell_exec("$php artisan migrate");
+
