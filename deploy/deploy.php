17545d43f94927481e75884be436388d22f176ec
rela589n <zsidelnik@gmail.com>
2021-06-02 22:46:23 +0300

    build(deploy): tweak deploy script to display output

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -4,11 +4,13 @@
 $php = 'php7.4';
 $composer = './composer.phar';
 
-shell_exec('cd ~/app');
-shell_exec("$php $composer install --no-ansi --no-progress --no-interaction");
-shell_exec("$php $composer dump-autoload");
-shell_exec("$php artisan storage:link");
-shell_exec("$php artisan nova:publish");
+echo
+shell_exec('cd ~/app'),
+shell_exec("$php $composer install --no-ansi --no-progress --no-interaction"),
+shell_exec("$php $composer dump-autoload"),
+shell_exec("$php artisan migrate --force"),
+shell_exec("$php artisan storage:link"),
+shell_exec("$php artisan nova:publish"),
 shell_exec("$php artisan view:clear");
-shell_exec("$php artisan migrate --force");
+
 
